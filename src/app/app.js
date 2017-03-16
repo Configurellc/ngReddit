543edfbf2636b96cae605bbfb3ef2dcbf069fc8d
Joshua Lee <joshualee@iconfigure.org>
2017-03-15 23:58:02 -1000

    updated placement of folders and deleted extras, can now load default page

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,28 @@
 import angular from 'angular';
-
+import * as uiRouter from 'angular-ui-router';
+import {DefaultState, DefaultCtrl} from './default';
 import '../style/app.css';
 
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
     controllerAs: 'app'
   }
 };
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
-
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['ui.router'])
+  .config(($stateProvider) => {
+    $stateProvider
+      .state(DefaultState.name, DefaultState)
+  })
+
+   .run(($state) => {
+    $state.go('default');
+  })
+
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('DefaultCtrl', DefaultCtrl)
 
 export default MODULE_NAME;
